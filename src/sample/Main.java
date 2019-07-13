91d93f5c63bf4756d1d8b7100ecc3eb8f32fc071
hussain <m.hussainul.islam@gmail.com>
2019-07-13 18:54:53 -0400

    heading added

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,33 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        BorderPane mainPane = new BorderPane();
+
+        GridPane quizPane = new GridPane();
+        mainPane.setCenter(quizPane);
+
+        Label heading = new Label("Two randomly generated numbers are: ");
+        quizPane.add(heading, 0, 0);
+        GridPane.setColumnSpan(heading, 2);
+
+        Label labelAddition = new Label("What is the addition of ");
+        TextField textAddition = new TextField();
+        quizPane.add(labelAddition, 0, 1);
+
+
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(mainPane, 300, 275));
         primaryStage.show();
     }
 
